a185535595a9f8fd63f1493872af4ead6656ecfd
Collin Mullis <cmullis@saintjoe.edu>
2015-04-03 02:30:41 -0400

    Added top comment and changed "stock" variable to 102 eggs

--- counter.js
+++ counter.js
@@ -1,4 +1,10 @@
-var stock = "1 lemon, 2 cabbages, and 101 eggs";
+/* Code Modified by Collin Mullis
+	CMP237 
+	Modifying Code
+	Second Hourly Exam
+*/
+// Variable used for matching
+var stock = "1 lemon, 2 cabbages, and 102 eggs";
 function minusOne(match, amount, unit) {
   amount = Number(amount) - 1;
   if (amount == 1) // only one left, remove the 's'
