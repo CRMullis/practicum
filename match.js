4d1a6236586bffdae3997f7761e98fdbb37970f4
Collin Mullis <cmullis@saintjoe.edu>
2015-04-03 02:48:27 -0400

    Added changed to RegExp to check for only 2 digit numbers

--- match.js
+++ match.js
@@ -5,7 +5,7 @@
 */
 // Changed input variable
 var input = "A string with 7 numbers in it... 42 and 88, 123 and 16, 4 and 22.";
-var number = /\b(\d+)\b/g;
+var number = /\b(\d\d)\b/g;
 var match;
 while (match = number.exec(input))
   console.log("Found", match[1], "at", match.index);
