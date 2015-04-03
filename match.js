025667afd78a0dca3f8632e1035e91a2223f7765
Collin Mullis <cmullis@saintjoe.edu>
2015-04-03 02:46:26 -0400

    Added top comment and modified input variable

--- match.js
+++ match.js
@@ -1,4 +1,10 @@
-var input = "A string with 3 numbers in it... 42 and 88.";
+/* Code modified by Collin Mullis
+	CMP237
+	Modifying Code
+	Second Hourly Exam
+*/
+// Changed input variable
+var input = "A string with 7 numbers in it... 42 and 88, 123 and 16, 4 and 22.";
 var number = /\b(\d+)\b/g;
 var match;
 while (match = number.exec(input))
